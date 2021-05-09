4bd1da4adf487b5dc9d6848d7196c319728809c8
David Andersson <nderssonpublic@gmail.com>
2021-05-09 14:49:58 +1000

    define CLI

--- data_model_formation/cli.py
+++ data_model_formation/cli.py
@@ -1,10 +1,21 @@
 """Command line interface for DataModelFormation."""
 
+import io
+import typing
+
 import click
+
+_FORMATS = typing.Literal["SQLALCHEMY", "DATACLASSES", "PYNAMODB"]
 
 
 @click.command()
-@click.argument("name")
-def main():
+@click.argument("path", type=click.File("rt"))
+@click.argument(
+    "format_",
+    type=click.Choice(typing.get_args(_FORMATS), case_sensitive=False),
+)
+def main(path: io.TextIOWrapper, format_: _FORMATS):
     """CLI entry point."""
+    print(path.read())
+    print(format_)
     print("hi")
